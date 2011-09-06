b79aba4c64b1250ee007f0962a164d008a9b80b8
inf0rmer <inf0rmer.realm@gmail.com>
2011-09-06 16:53:47 +0100

    Initial implementation of Collection filtering methods written and unit tested

--- assets/js/core.js
+++ assets/js/core.js
@@ -34,69 +34,79 @@
 		/**
 		 * Filter collection by weight.
 		 *
-		 * @method getByWeight
-		 * @param {String} weightArray Array of weights to search for.
+		 * @method filterByWeight
+		 * @param {Number|String} targetWweight Weight to search for
 		 * Formats accepted: 
 		 * 		50 		: will return all items with weight = 50
 		 *		'10-50' : will return all items with weight between 10 and 50
 		 *
 		 * @returns {Array} An array with the filtered collection objects
 		 */
-		getByWeight: function(weightArray) {
-			
+		filterByWeight: function(targetWeight) {
+			return this.filter(function(answer) {
+				return answer.get("metadata").weight === targetWeight;
+			});
 		},
 		
 		/**
 		 * Filter collection by objects that have images.
 		 *
-		 * @method getHasImage
+		 * @method filterByImage
 		 *
 		 * @returns {Array} An array with the filtered collection objects
 		 */
-		getHasImage: function() {
-		
+		filterByImage: function() {
+			return this.filter(function(answer) {
+				return answer.get("image") && answer.get("image") != '';
+			});
 		},
 		
 		/**
-		 * Filter collection by objects that have translations in the given languages.
+		 * Filter collection by objects that are authored in a given language.
 		 *
-		 * @method getHasLanguage
-		 * @param {Array} languageArray Locale strings to filter collection by, ie. ['pt-PT', 'en-US'].
-		 * @param {String} method Method to filter collection by. Accepts 'OR' or 'AND', default is 'AND'.
+		 * @method filterByLanguage
+		 * @param {String} language Locale string to filter collection by, ie. 'pt-PT'.
 		 *
 		 * @returns {Array} An array with the filtered collection objects
 		 */
-		getHasLanguage: function(languageArray, method) {
-		
+		filterByLanguage: function(language) {
+			return this.filter(function(answer) {
+				return _.indexOf(languageArray, answer.get('metadata').language) != -1;
+			});
 		},
 		
 		/**
 		 * Filter collection by objects that have given user types as authors.
 		 *
-		 * @method getByuserType
-		 * @param {Array} userTypeArray User types to filter collection by, ie. ['designer', 'developer'].
-		 * @param {String} method Method to filter collection by. Accepts 'OR' or 'AND', default is 'AND'.
+		 * @method filterByUserType
+		 * @param {String} userType User types to filter collection by.
 		 *
 		 * @returns {Array} An array with the filtered collection objects
 		 */
-		getByuserType: function(userTypeArray, method) {
-		
+		filterByUserType: function(userType) {
+			return this.filter(function(answer) {
+				return userTypeArray answer.get('metadata').usertype == userType;
+			});
 		},
 		
 		/**
 		 * Filter collection by objects that were created on given dates.
 		 *
-		 * @method getByCreated
-		 * @param {Array} dateArray Dates to filter collection by, ie ['Friday, September 2 2011', '2011-09-03'].
+		 * @method filterByCreated
+		 * @param {String} fromDate Starting date to filter collection by, ie 'Friday, September 2 2011'.
+		 * @param {String} toDate 	Ending date to filter collection by, ie '2011-09-06'.
 		 * Formats accepted: Pretty much anything Date.parse() can understand.
 		 * See: https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Date/parse
 		 *
 		 * @returns {Array} An array with the filtered collection objects
 		 */
-		getByCreated: function(dateArray) {
-		
-		}
-		
+		filterByCreated: function(fromDate, toDate) {
+			if (!toDate) toDate = fromDate;
+						
+			return this.filter(function(answer) {
+				return new Date(answer.get('metadata').date) >= new Date(fromDate) && new Date(answer.get('metadata').date) <= new Date(toDate);
+			});
+		}		
 	});
 	
 	// Instantiate AnswerList collection
