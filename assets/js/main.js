53b5ad3ceb45da9c6bc48b58d41580bc82017858
inf0rmer <inf0rmer.realm@gmail.com>
2011-10-04 15:03:20 +0100

    testing postcommit hook

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+require([], function() {
+	require([
+		'libs/underscore.min',
+		'libs/jquery-1.6.3.min',
+		'libs/handlebars',
+		'libs/backbone-0.5.3.min',
+		'libs/backbone-localstorage',
+		'libs/strftime',
+		'core',
+		'questions',
+		'answers',
+		'simpleViz'
+	], 
+	function(){
+	
+	});
+});
